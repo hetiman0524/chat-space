6b5c651c2c8c7443434c28ad9cda4969a0d2373d
hetiman0524 <heti0524.jdi@gmail.com>
2019-11-03 20:48:47 +0900

    add ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,45 @@
+$(function(){
+        function buildHTML(message){
+                
+                var content_tag = message.content.length > 0 ? 
+                 `<p class = "lower-message__content">${message.content}</p>` : "";
+                var image_tag = message.image.url ? 
+                 `<img src = ${message.image.url} ,class = "lower-message__image">`: "";
+
+                var html = ` <div class = "message">
+                                <div class = "essage__upper-info"></div>
+                                   <p class = "message__upper-info__talker"> ${message.name}</p>
+                                   <p class = "message__upper-info__date"> ${message.created_at}</p>
+                                <p class = "message__text">
+                                   ${content_tag }
+                                   ${image_tag }
+                                </p>`
+                return html;
+        }
+
+        $('#new_message').on('submit', function(e){
+                e.preventDefault();
+                console.log(this)
+                var formData = new FormData(this);
+                var url = $(this).attr('action ')
+                $.ajax({
+                        url: url,
+                        type: 'POST', 
+                        data: formData,  
+                        dataType: 'json',
+                        processData: false,
+                        contentType: false
+                })
+                .done(function(data){
+                        var html = buildHTML(data);
+                        $('.messages').append(html);
+                        $('.form__message').val('');
+                        $('.form__submit').removeAttr('disabled');
+                        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+                        return false
+                })
+                .fail(function() {
+                        alert("メッセージ送信に失敗しました");
+                 });
+        })
+});
