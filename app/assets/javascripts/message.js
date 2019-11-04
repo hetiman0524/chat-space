51aa99cdbd608241000ed04162606ac38f9955cc
hetiman0524 <heti0524.jdi@gmail.com>
2019-11-04 12:37:33 +0900

    ajax add 2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -33,9 +33,9 @@ $(function(){
                 .done(function(data){
                         var html = buildHTML(data);
                         $('.messages').append(html);
-                        $('.form__message').val('');
                         $('.form__submit').removeAttr('disabled');
                         $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+                        $('form')[0].reset();
                         return false
                 })
                 .fail(function() {
